2cd2f2af78de84cd6c4ef23f7b4eec3d05cdd962
yopiti <david.klement@gmail.com>
2024-10-23 12:55:45 +0200

    Version 0.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-email-to-github-issues',
-    version='0.9',
+    version='0.9.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
